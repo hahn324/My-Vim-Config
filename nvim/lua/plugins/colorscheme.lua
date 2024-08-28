568ce1b8f2df718b68da19b78d8c2f291e699d39
Harrison Hahn <harrisonhahn96@gmail.com>
2024-08-28 15:41:23 -0400

    Fix border update being outside opts

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,10 +4,10 @@ return {
 		lazy = true,
 		opts = {
 			style = "moon",
+			on_colors = function(colors)
+				colors.border = "#1d5380"
+			end,
 		},
-		on_colors = function(colors)
-			colors.border = "#1d5380"
-		end,
 	},
 	{
 		"LazyVim/LazyVim",
